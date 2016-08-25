b5f05b6eab136c0450d684f56eb12d66b6e4e335
Paulo Giacomelli <paulogiacomelli@paulos-imac.gs.umt.edu>
2016-08-25 13:56:41 -0600

    Website Setup

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 2200);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("mypage").style.display = "block";
+
+
+  $(function(){
+  $("#typed").typed({
+    // strings: ["Paulo Giacomelli", "a computer science student", "tech enthusiast", "retired college basketball player", "not that cool", "ready to help you though."],
+    typeSpeed: 0
+  });
+});
+  
+}

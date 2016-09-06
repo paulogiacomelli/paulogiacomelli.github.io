c85b37ac95c47dccf00780695d7f87f1962cd329
Paulo Giacomelli <paulogiacomelli@paulos-imac.gs.umt.edu>
2016-09-06 11:55:31 -0600

    Updated Css for Responsive Footer/Header

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -5,17 +5,18 @@ function myFunction() {
 }
 
 function showPage() {
-  document.getElementById("loader").style.display = "none";
-  document.getElementById("mypage").style.display = "block";
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("mypage").style.display = "block";
 
 
-  $(function(){
-  $("#typed").typed({
-    // strings: ["Paulo Giacomelli", "a computer science student", "tech enthusiast", "retired college basketball player", "not that cool", "ready to help you though."],
-    typeSpeed: 0
-  });
-});
-  
+    $(function() {
+        $("#typed").typed({
+            // strings: ["Paulo Giacomelli", "a computer science student", "tech enthusiast", "retired college basketball player", "not that cool", "ready to help you though."],
+            typeSpeed: 0
+        });
+    });
+
 }
+
 
 
